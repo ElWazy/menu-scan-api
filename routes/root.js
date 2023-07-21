d4d64c22ee79adac30a89c6463006b21a5394e3b
ElWazy <ElCazero.Fer@gmail.com>
2023-07-20 23:42:54 -0400

    format: `npm run lint` :wink:

--- routes/root.js
+++ routes/root.js
@@ -1,6 +1,5 @@
 export default async function (fastify, opts) {
-  
   fastify.get('/', async function (request, reply) {
-    return { 'message': 'Welcome to Menu Scan API!' }
+    return { message: 'Welcome to Menu Scan API!' }
   })
 }
